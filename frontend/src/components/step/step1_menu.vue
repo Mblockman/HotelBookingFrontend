67f2885e219146f5da2f4a9e7ff4a48fce69add3
Mblockman <drmdim@naver.com>
2023-11-15 21:32:56 +0900

    Add files via upload

--- frontend/src/components/step/step1_menu.vue
+++ frontend/src/components/step/step1_menu.vue
@@ -1,4 +1,5 @@
 <template>
+  {{ dataChanged() }}
     <section class="home" id="home">
         <div class="container">
           <h1>Make Memories</h1>
@@ -7,10 +8,18 @@
           <div class="content grid">
             <div class="box">
               <span>ROOM TYPE </span> <br>
-              <input type="string" placeholder="29/20/2021">
+              <input v-model="source" list="newssources-list" v-on:input="sourceChanged" 
+              name="source-selection" placeholder="Please specify news source ..."/>
+              <datalist id="newssources-list">
+                <option v-for="source in sources" v-bind:value="source.rtRoomType" v-bind:key="source.rtRoomTypeID"></option>
+              </datalist>
             </div>
             <div class="box">
-              <span>STAY PERRIOD </span> <br>
+              <span>ARRIVAL DATE </span> <br>
+              <input type="date" placeholder="29/20/2021">
+            </div>
+            <div class="box">
+              <span>DEPARTURE DATE </span> <br>
               <input type="date" placeholder="29/20/2021">
             </div>
             <div class="box">
@@ -19,7 +28,7 @@
             </div>
             <div class="box">
               <span>CHILDREN </span> <br>
-              <input type="number" placeholder="01">
+              <input type="number" placeholder="0">
             </div>
             <div class="box">
               <button class="flex1">
@@ -32,10 +41,37 @@
       </section>
     
 </template>
+
 <script>
+let once = false;
+
 export default {
-    name : "Step1_menu"
-}
+    //name : "Step1_menu",newssources-list
+    name : "newssources-list",
+    data () {
+          return {
+            sources: [],
+            source: '',
+            deepSource: ''
+          }
+        },
+    methods: {
+      dataChanged() {
+        if (once == true) {
+          return;
+        }
+        once = true;
+        this.$axios.get("http://localhost:5000/api/roomtypes").then((response) => {           
+            for (var i=0; i<response.data.length; i++){
+              this.sources.push(response.data[i]);
+            }
+            console.log(this.sources);
+           
+        });
+      },          
+    },
+  }
+
 </script>
 
 
